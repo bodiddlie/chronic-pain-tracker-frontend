c1a056c92f82486d0887f83cdec8e3c82c2c633a
Nick Klepinger <bodiddlie@gmail.com>
2020-05-01 22:20:43 -0700

    Calendar (#2)
    * beginning of calendar, still have render loop
    
    * fixed render loop
    
    * got routing working, added prettier config
    
    * add react-icons, wire up next/prev links, turn days into links, styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { format } from 'date-fns';
+
+import { Calendar } from './calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={`/${format(new Date(), 'yyyy-MM-dd')}`} />}
+        />
+        <Route path="/:dayKey" element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
